1584a66b123ad6ebdba1417e6576e402d6451a16
Marc-André Brochu <marcandrebrochu@fastmail.com>
2026-05-06 18:45:22 -0400

    Split into tests/ and src/ directories

---
+++ src/FlashcardService.API/Controllers/DeckController.cs
@@ -0,0 +1,33 @@
+using FlashcardService.Application.Common.Dtos;
+using FlashcardService.Application.Decks.Commands.CreateDeck;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashcardService.API.Controllers;
+
+[ApiController]
+[Route("decks")]
+public sealed class DeckController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DeckDto>>> GetAllDecks()
+    {
+        return Ok((IEnumerable<DeckDto>)[]);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<DeckDto>> GetDeckById(Guid id)
+    {
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DeckDto>> CreateDeck([FromBody] CreateDeckCommand request)
+    {
+        var result = await mediator.Send(request);
+        
+        const string routeName = nameof(GetDeckById);
+        var routeValues = new { id = Guid.NewGuid() }; // fills in the {id} slot in the get url (i.e. routeName)
+        return CreatedAtAction(routeName, routeValues, result.Value);
+    }
+}
